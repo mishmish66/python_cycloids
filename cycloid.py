bddd5b9b9bb72545788b461dd6679713f4560371
mishmish66 <m.lvovsky@protonmail.com>
2022-01-25 22:14:49 -0500

    something does not work but it does animate sort of

--- cycloid.py
+++ cycloid.py
@@ -1,5 +1,8 @@
+from tkinter.ttk import setup_master
 from utils import *
 import math
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 class Cycloid:
     def __init__(self, pin_count, tooth_dif, pinwheel_r, pin_r, eccentricity, offset_angle):
@@ -10,14 +13,76 @@ class Cycloid:
         self.eccentricity = eccentricity
         self.offset_angle = offset_angle
 
-    def get_point_from_wobbles(self, draw_wobbles, input_wobbles):
+    def get_point_from_wobbles(self, draw_wobbles, input_wobbles, twist):
         center_pos = np.matmul(get_rot_mat(input_wobbles*2*math.pi), vert([self.eccentricity, 0]))
-        wobble_pos = np.matmul(get_rot_mat(draw_wobbles*2*math.pi + self.offset_angle), vert([-self.eccentricity, 0]))
-        rotation_pos = np.matmul(get_rot_mat(draw_wobbles*2*math.pi * self.get_rot_per_wobble() + self.offset_angle), vert([self.pinwheel_r, 0]))
+        wobble_pos = np.matmul(get_rot_mat(draw_wobbles*2*math.pi + self.offset_angle + twist), vert([self.eccentricity, 0]))
+        rotation_pos = np.matmul(get_rot_mat(draw_wobbles*2*math.pi * self.get_rot_per_wobble() + self.offset_angle + twist), vert([self.pinwheel_r, 0]))
         return center_pos + wobble_pos + rotation_pos
 
     def get_rot_per_wobble(self):
-        return self.tooth_dif/self.pin_count
+        return -self.tooth_dif/self.pin_count
+
+class Cycloid_Drawer:
+    def __init__(self, cycloid, steps = 1024):
+        self.steps = steps
+        self.cycloid = cycloid
+            
+    def plot_cycloid(self, ax, steps = 1024, input_wobbles = 0):
+        points = self.cycloid.get_points(steps, input_wobbles)
+        ax.plot(points[0], points[1])
     
-    def get_points(self, steps, input_wobbles):
-        pass # TODO
+    def get_points(self, input_wobbles = 0, steps = 1024, twist = None):
+        if twist == None:
+            twist = self.cycloid.offset_angle + input_wobbles * self.cycloid.get_rot_per_wobble()*2*math.pi
+        points_x = np.empty(int(steps))
+        points_y = np.empty(int(steps))
+
+        wobbles = self.cycloid.get_rot_per_wobble()**-1
+        wobbles_per_step = wobbles/steps
+
+        for i in range(0, steps):
+            point = self.cycloid.get_point_from_wobbles(wobbles_per_step*i, input_wobbles, twist)
+            points_x[i] = point[0]
+            points_y[i] = point[1]
+        
+        return [points_x, points_y]
+
+class Cycloid_Animator:
+    def __init__(self, drawer, starting_wobbles = 0, wobble_step = 0.01, ending_wobbles = None):
+        self.drawer = drawer
+        self.starting_wobbles = starting_wobbles
+        self.wobble_step = wobble_step
+        if ending_wobbles == None:
+            ending_wobbles = starting_wobbles + self.drawer.cycloid.get_rot_per_wobble()**-1
+        self.ending_wobbles = ending_wobbles
+    
+    def get_cycloid_points_temporal(self):
+        steps = int((self.ending_wobbles - self.starting_wobbles)/self.wobble_step)
+        points = np.empty([steps, 2, self.drawer.steps])
+
+        for step in range(0, steps):
+            points[step] = self.drawer.get_points(step*self.wobble_step, self.drawer.steps)
+
+        return points
+
+    def get_steps(self):
+        return int((self.ending_wobbles - self.starting_wobbles)/self.wobble_step)
+    
+
+    def animate(self, fig, ax):
+        points = self.get_cycloid_points_temporal()
+
+        line, = ax.plot([], [], lw = 2)
+
+        xdata, ydata = [], []
+
+        def init():
+            line.set_data([], [])
+            return line,
+
+        def animate(step):
+            this_line = points[step]
+            line.set_data(this_line[0], this_line[1])
+            return line,
+        
+        return animation.FuncAnimation(fig, animate, init_func=init, blit = True, frames = self.get_steps(), interval=1)
--- main.py
+++ main.py
@@ -4,36 +4,25 @@ import numpy as np
 from cycloid import *
 from utils import *
 
-steps = 1024
-
 tooth_dif = -1
-pins = 5
+pins = 4
 pinwheel_r = 1
 pin_r = 0.1
 eccentricity = 0.1
 
-rm = get_rot_mat(0)
-
-vec = np.vstack([1, 0])
-
-mult = np.matmul(rm, vec)
-
 c = Cycloid(pins, tooth_dif, pinwheel_r, pin_r, eccentricity, offset_angle=0)
 
-wobble_per_step = (c.get_rot_per_wobble())**-1/steps
+fig = plt.figure()
+ax = plt.axes()
 
-points_x = np.empty([steps])
-points_y = np.empty([steps])
+cd = Cycloid_Drawer(c, 256)
 
-for i in range(0, steps):
-    point = c.get_point_from_wobbles(wobble_per_step*i, 0)
+ca = Cycloid_Animator(cd, wobble_step=0.01)
 
-    points_x[i] = point[0]
-    points_y[i] = point[1]
+ani = ca.animate(fig, ax)
 
-fig, ax = plt.subplots(figsize=(2,2), constrained_layout=True)
-ax.plot(points_x, points_y)
+plt.xlim([-2, 2])
+plt.ylim([-2, 2])
 plt.axis('equal')
-plt.xlim(-2, 2)
-plt.ylim(-2, 2)
+
 plt.show()
